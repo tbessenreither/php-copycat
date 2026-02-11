273386f8b80567ef8d48ddd757fbb75c3b3788cd
Tobias Beßenreither <github@tobias.bessenreither.de>
2026-02-11 20:44:37 +0100

    added more Copy Targets;

--- src/Enum/CopyTargetEnum.php
+++ src/Enum/CopyTargetEnum.php
@@ -7,5 +7,9 @@ enum CopyTargetEnum: string
 {
 	case DDEV_COMMANDS_WEB = '.ddev/commands/web';
 	case DDEV_COMMANDS_HOST = '.ddev/commands/host';
+	case SYMFONY_BIN = 'bin';
+	case SYMFONY_CONFIG_PACKAGES = 'config/packages';
+	case SYMFONY_CONFIG_ROUTES = 'config/routes';
+	case PUBLIC = 'public';
 
 }
